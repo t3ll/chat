eb93dcabf388e11ff682f5bb733f1b0770c84fa8
Andreas Tell <54512748+t3ll@users.noreply.github.com>
2021-04-02 17:45:12 +0200

    Add example cowboy http & ws handler

--- server/apps/chat_web/src/chat_web_app.erl
+++ server/apps/chat_web/src/chat_web_app.erl
@@ -7,10 +7,21 @@
 
 -behaviour(application).
 
--export([start/2, stop/1]).
+-export([start/2, start_phase/3, stop/1]).
 
 start(_StartType, _StartArgs) ->
     chat_web_sup:start_link().
+
+start_phase(start_cowboy, _StartType, []) ->
+    Host = '_',
+    Routes = [{"/", cowboy_static, {priv_file, chat_web, "index.html"}},
+              {"/[...]", cowboy_static, {priv_dir, chat_web, ""}}],
+    Dispatch = cowboy_router:compile([{Host, Routes}]),
+    {ok, _} = cowboy:start_clear(chat_web_listener,
+        [{port, 8000}],
+        #{env => #{dispatch => Dispatch}}
+    ),
+    ok.
 
 stop(_State) ->
     ok.
--- server/apps/chat_ws/src/chat_ws_app.erl
+++ server/apps/chat_ws/src/chat_ws_app.erl
@@ -7,10 +7,19 @@
 
 -behaviour(application).
 
--export([start/2, stop/1]).
+-export([start/2, start_phase/3, stop/1]).
 
 start(_StartType, _StartArgs) ->
     chat_ws_sup:start_link().
+
+start_phase(start_cowboy, _StartType, []) ->
+    Host = '_',
+    Routes = [{"/websocket", chat_ws_h, []}],
+    Dispatch = cowboy_router:compile([{Host, Routes}]),
+	{ok, _} = cowboy:start_clear(http, [{port, 9000}], #{
+		env => #{dispatch => Dispatch}
+	}),
+    ok.
 
 stop(_State) ->
     ok.
